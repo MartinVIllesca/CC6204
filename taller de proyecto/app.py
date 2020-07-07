d6e93f99e066d1241350be6e3f3077f3846778b3
MartinVIllesca <valderramaillesca.m@gmail.com>
2020-07-07 11:04:14 -0400

    update

--- taller de proyecto/app.py
+++ taller de proyecto/app.py
@@ -8,11 +8,12 @@ from LOAD import *
 app = Flask(__name__, template_folder='templates')
 
 def init():
-    global model#,graph
+    global model,graph
     # load the pre-trained Keras model
     with tf.device('/cpu:0'):
-        model = load_model('model1/model.h5')
-    # graph = tf.get_default_graph()
+        model = load_model('./model/model.05-3.42.h5')
+    # print(model.summary())
+    graph = tf.Graph()
 
 @app.route('/')
 def upload_file():
@@ -26,20 +27,21 @@ def upload_image_file():
         # print(img)
         im2arr = np.array(img)
         im2arr = im2arr.reshape(1,150,150,3)
-        # with graph.as_default():
-        y_pred = model.predict_classes(im2arr)
-        clases = ['blenheim_spaniel',
-                  'chihuahua',
-                  'japanses spaniel',
-                  'maltese',
-                  'papillon',
-                  'pekinese',
-                  'shih-tzu',
-                  'toy_terrier']
-        return 'Predicted Number: ' + clases[y_pred[0]]
+        print(im2arr.shape)
+        with graph.as_default():
+            y_pred = model.predict_classes(im2arr)
+        # clases = ['blenheim_spaniel',
+        #           'chihuahua',
+        #           'japanses spaniel',
+        #           'maltese',
+        #           'papillon',
+        #           'pekinese',
+        #           'shih-tzu',
+        #           'toy_terrier']
+        return 'Predicted Number: ' + str(y_pred[0])
 		
 if __name__ == '__main__':
     print(("* Loading Keras model and Flask starting server..."
         "please wait until server has fully started"))
     init()
-    app.run(debug = True)
+    app.run(debug = True, threaded=False)
